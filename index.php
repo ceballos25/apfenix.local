f21edbdfafbc9ef9a9e377db3123f22755dcac86
Cristian Ceballos <ceballosmarincristiancamilo@gmail.com>
2026-06-25 14:01:29 -0500

    bar

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once "config/config.php";
 require_once "includes/coupon.php";
-$porcentaje_venta = 46.6;
+$porcentaje_venta = 47.1;
 $couponActive = CouponHelper::isActive();
 ?>
 <!doctype html>
